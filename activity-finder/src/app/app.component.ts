72b60dff69d18737eed9c74eb380da762f4ce8af
Eduardo Valias <edupv123@hotmail.com>
2021-12-06 17:17:27 -0300

    Add search by type and key

--- activity-finder/src/app/app.component.ts
+++ activity-finder/src/app/app.component.ts
@@ -12,17 +12,27 @@ export class AppComponent {
   constructor(private http : HttpClient){}
   
   title = 'activity-finder';
-
   showUI = true;
   readonly apiURL = 'http://www.boredapi.com/api/';
 
-  search(){
+  displayVal = '';
+
+  allData: Activity[] = [];
+
+  search(type: string, key:string){
     this.showUI = false;
-   // this.http.get('http://www.boredapi.com/api/activity/').subscribe(data => {
-   //  console.log(data);
-   // });
-   this.http.get<Activity[]>(`${ this.apiURL }activity/`)
-            .subscribe(data => console.log(data));
-    
+
+    if(key != '') {
+      this.http.get<Activity[]>(`${ this.apiURL }activity?key=${ key }`)
+              .subscribe(data => console.log(data));
+    } 
+    else if(type != ''){
+      this.http.get<Activity[]>(`${ this.apiURL }activity?type=${ type }`)
+              .subscribe(data => console.log(data));
+    } 
+    else {
+      this.http.get<Activity[]>(`${ this.apiURL }activity/`)
+              .subscribe(data => console.log(data));
+    }  
   }
 }
