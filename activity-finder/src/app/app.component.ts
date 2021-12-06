c46de9780340d6556a90f185ac4076e9fb44d08b
Eduardo Valias <edupv123@hotmail.com>
2021-12-06 15:40:49 -0300

    Getting information from API

--- activity-finder/src/app/app.component.ts
+++ activity-finder/src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Activity } from '../models/activity.model';
 
 @Component({
   selector: 'app-root',
@@ -6,12 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private http : HttpClient){}
+  
   title = 'activity-finder';
 
   showUI = true;
+  readonly apiURL = 'http://www.boredapi.com/api/';
 
   search(){
     this.showUI = false;
+   // this.http.get('http://www.boredapi.com/api/activity/').subscribe(data => {
+   //  console.log(data);
+   // });
+   this.http.get<Activity[]>(`${ this.apiURL }activity/`)
+            .subscribe(data => console.log(data));
     
   }
 }
